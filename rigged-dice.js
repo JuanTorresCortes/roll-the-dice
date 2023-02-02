c4051a3129810cc2a41edbdbb0d28d7dd17f02b2
Juan <juan.giana8589@gmail.com>
2023-02-02 10:17:04 -0600

    added an extra side on line 9

--- rigged-dice.js
+++ rigged-dice.js
@@ -2,10 +2,10 @@ const prompt = require('prompt-sync')({sigint: true});
 // 3 is the rigged dice 
 let pro = prompt("To roll the dice hit the Enter key: ")
 
-let roll = (Math.floor(Math.random() * 7) + 1);
+let roll = (Math.floor(Math.random() * 8) + 1);
 
 if(roll === 1 || roll === 2 || roll === 4 || roll === 5 || roll === 6 || roll === 7){
     console.log(Math.floor(Math.random() * 5) + 1)    
-}else if(roll === 7){
+}else if(roll === 7 || roll === 8){
     console.log(Math.floor(Math.random() * 5) + 1) 
 }else{console.log(3)}
